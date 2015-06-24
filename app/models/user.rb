2fd1bf9555c92f7f57cfb72d09f5358361d27b95
Chris Luhring <cluhring@gmail.com>
2015-06-24 15:46:23 -0600

    fixed should_not allow_value tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :first_name, :last_name, presence: true, length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 50 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, }
+                    format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true,
                        length: { minimum: 6 }
   has_secure_password
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -17,30 +17,31 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of(:email).on(:create)
   should validate_length_of(:email).is_at_most(50)
   should validate_uniqueness_of(:email)
-  should allow_value('person@email.com',
-                     'person.name@email.com',
-                     'person_name@email.com'
-                     ).for(:email)
-  should_not allow_value('person@email,com',
-                         'person@email',
-                         'person.com',
-                         'person'
-                         ).for(:email)
+  should allow_value('person@email.com').for(:email)
+  should allow_value('person.name@email.com').for(:email)
+  should allow_value('person_name@email.com').for(:email)
+  should_not allow_value('person.com').for(:email)
+  should_not allow_value('person').for(:email)
+  should_not allow_value('person@email..com').for(:email)
+  should_not allow_value('person@email,com').for(:email)
 
   should have_secure_password
   # should validate_length_of(:password).is_equal_to(7)
   should validate_length_of(:password).is_at_least(6)
-  # should validate_uniqueness_of(:password)
 
   def setup
-    @user = User.create(first_name: "First",
-                     last_name: "Last",
-                     email: "user@example.com",
-                     password: "zoinks!",
-                     password_confirmation: "zoinks!")
+    @valid_user = User.create(first_name: "First",
+                        last_name: "Last",
+                        email: "uSer@exaMple.com",
+                        password: "zoinks!",
+                        password_confirmation: "zoinks!")
   end
 
-  test "should be valid" do
-    assert @user.valid?
+  test "a user should be valid" do
+    assert @valid_user.valid?
+  end
+
+  test "an email should be saved as lower-case" do
+    assert_equal "user@example.com", @valid_user.email
   end
 end
